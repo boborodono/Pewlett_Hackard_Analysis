29786fc9f277aa136a68a3c54ee7715686f64ec9
boborodono <bowen.wilder@GMAIL.COM>
2021-12-12 16:22:12 -0600

    Finished Deliverables, Working on README

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,10 +1,4 @@
 -- Query Tables
-SELECT * FROM employees;
-SELECT * FROM titles;
-SELECT * FROM impending_retirement;
-
-DROP impending_retirement CASCADE;
-
 -- DELIVERABLE 1
 -- Organize a database to capture the details of upcoming retirees
 SELECT 
@@ -14,9 +8,48 @@ SELECT
 	t.title,
 	t.from_date,
 	t.to_date
-INTO impending_retirement
+INTO impending_retirement -- Export as csv
 FROM employees AS e
 INNER JOIN titles AS t 
 	ON e.emp_no = t.emp_no
 	WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no;
+
+-- Make a table to hold a list of the employees who are retiring by title
+SELECT DISTINCT ON (ir.emp_no)
+	ir.emp_no,
+	ir.first_name,
+	ir.last_name,
+	ir.title
+INTO unique_titles -- Export as csv
+FROM impending_retirement as ir
+ORDER BY ir.emp_no, ir.to_date DESC;
+
+-- Retrieve the number of employees who are retiring by title and count the number of titles
+SELECT COUNT(ut.title),
+	ut.title
+INTO retiring_titles -- Export as csv
+FROM unique_titles as ut
+GROUP BY ut.title 
+ORDER BY COUNT (ut.title) DESC;
+
+
+-- Deliverable 2
+-- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_program -- Export as csv
+FROM employees as e
+INNER JOIN dept_emp as de
+	ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+	ON e.emp_no = t.emp_no
+WHERE (de.to_date = ('9999-01-01')) 
+	AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 ORDER BY e.emp_no;
